54bfaaa65aff091a726131f26b50d41be49c2593
sanderjn1 <sanderjn@uia.no>
2023-04-21 09:30:20 +0000

    pls

--- tcp_client.go
+++ tcp_client.go
@@ -4,17 +4,18 @@ import (
 	"net"
 	"log"
 	"os"
+	"github.com/sanderjn1/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.3:8100")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
-	log.Println("os.Args[1] = ", os.Args[1])
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 
- 	_, err = conn.Write([]byte(os.Args[1]))
 	if err != nil {
 		log.Fatal(err)
 	}
